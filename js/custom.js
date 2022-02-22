3a955ebb060529359bbe271d9f5f39d1e9909ff2
Mannu Bansal <mannubans@gmail.com>
2022-02-22 21:52:27 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -42,14 +42,14 @@
   });
 
   // menu fixed js code
-  // $(window).scroll(function () {
-  //   var window_top = $(window).scrollTop() + 1;
-  //   if (window_top > 50) {
-  //     $('.main_menu_iner').addClass('menu_fixed animated fadeInDown');
-  //   } else {
-  //     $('.main_menu_iner').removeClass('menu_fixed animated fadeInDown');
-  //   }
-  // });
+  $(window).scroll(function () {
+    var window_top = $(window).scrollTop() + 1;
+    if (window_top > 50) {
+      $('.main_menu_iner').addClass('menu_fixed animated fadeInDown');
+    } else {
+      $('.main_menu_iner').removeClass('menu_fixed animated fadeInDown');
+    }
+  });
 // Owl Carousal
 $("#owl-slider").owlCarousel({
   items: 1,
